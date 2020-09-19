846495a828532504749ac8d6d7f0fad9f14c44f4
Nishant Sharma <4unishant@gmail.com>
2020-09-19 16:27:51 +0530

    signup error resolved

--- reset.js
+++ reset.js
@@ -9,55 +9,46 @@ var formdiv=document.createElement("div");
 formdiv.setAttribute("class","container-fluid");
 
 var form=document.createElement("form");
-form.setAttribute("onsubmit","signup();return false;");
-var fname=document.createElement("input");
-fname.setAttribute("type","text");
-fname.setAttribute("id","fname");
-fname.setAttribute("required",true);
-fname.setAttribute("class","form-control");
-fname.setAttribute("placeholder","ENTER FIRST NAME");
-var br1=document.createElement("br");
-var lname=document.createElement("input");
-lname.setAttribute("type","text");
-lname.setAttribute("id","lname");
-lname.setAttribute("required",true);
-lname.setAttribute("class","form-control");
-lname.setAttribute("placeholder","ENTER LAST NAME");
-var br2=document.createElement("br");
+form.setAttribute("onsubmit","reset();return false;");
 var email=document.createElement("input");
 email.setAttribute("type","text");
 email.setAttribute("id","email");
 email.setAttribute("required",true);
 email.setAttribute("class","form-control");
 email.setAttribute("placeholder","ENTER EMAIL");
+var br1=document.createElement("br");
+var newpwd=document.createElement("input");
+newpwd.setAttribute("type","password");
+newpwd.setAttribute("id","newpwd");
+newpwd.setAttribute("required",true);
+newpwd.setAttribute("class","form-control");
+newpwd.setAttribute("placeholder","ENTER NEW PASSWORD");
+var br2=document.createElement("br");
+var cnfpwd=document.createElement("input");
+cnfpwd.setAttribute("type","password");
+cnfpwd.setAttribute("id","cnfpwd");
+cnfpwd.setAttribute("required",true);
+cnfpwd.setAttribute("class","form-control");
+cnfpwd.setAttribute("placeholder","ENTER PASSWORD");
 var br3=document.createElement("br");
-var pwd=document.createElement("input");
-pwd.setAttribute("type","password");
-pwd.setAttribute("id","pwd");
-pwd.setAttribute("required",true);
-pwd.setAttribute("class","form-control");
-pwd.setAttribute("placeholder","ENTER PASSWORD");
-var br4=document.createElement("br");
 var button=document.createElement("button");
 button.setAttribute("class","btn btn-primary");
 button.setAttribute("type","submit");
 button.innerText="SIGN UP";
 formdiv.appendChild(form);
-form.appendChild(fname);
-form.appendChild(br1);
-form.appendChild(lname);
-form.appendChild(br2);
 form.appendChild(email);
+form.appendChild(br1);
+form.appendChild(newpwd);
+form.appendChild(br2);
+form.appendChild(cnfpwd);
 form.appendChild(br3);
-form.appendChild(pwd);
-form.appendChild(br4);
 form.appendChild(button);
 
 maindiv.appendChild(h1);
 maindiv.appendChild(formdiv);
 document.body.appendChild(maindiv);
 
-async function signup()
+async function reset()
 {
     try
     {
